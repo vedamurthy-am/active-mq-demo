5d9e1687bd6388dc6c4aac7e0fa8afd9d90182fd
vedamurthy-am <vedamurthy.am@gmail.com>
2021-07-18 22:04:33 +0530

    added

---
+++ src/test/java/org/veda/activemqdemo/ActiveMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.veda.activemqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
